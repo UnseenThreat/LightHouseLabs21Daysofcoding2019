9ac31023c43abbf36d4e2be9026770e5142ca305
UnseenThreat <vawogbemi@gmail.com>
2019-05-06 22:58:44 -0400

    day 3

--- code.js
+++ code.js
@@ -11,3 +11,23 @@ function countModules(){
   return availableModules.length;
   
 }
+
+//Day 3
+
+function countEssential(){
+  var num = 0;
+  var count = 0;
+  
+  while ( num < countModules() ){
+    
+    if (availableModules[num].essential === true){
+     
+      count++;
+      
+    }
+    num++;
+  }
+  
+  return count;
+  
+}
