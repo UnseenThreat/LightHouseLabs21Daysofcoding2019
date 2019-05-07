51dac4b133ff4ff3eb3459a1f5f7297f04eed393
UnseenThreat <vawogbemi@gmail.com>
2019-05-06 23:12:45 -0400

    day 4

--- code.js
+++ code.js
@@ -31,3 +31,33 @@ function countEssential(){
   return count;
   
 }
+
+//Day 4
+
+function loadModule(index){
+  var module = availableModules[index];
+  
+  module.enabled = true;
+  
+  ship.modules.push(module);
+}
+  
+function findModule(string){
+  var num = 0;
+  
+  
+  while ( num < countModules() ){
+    
+    if (availableModules[num].name == string){
+     
+      return num;
+      
+    }
+    num++;
+  }
+  
+  return null;
+  
+}
+
+loadModule(findModule("life-support"))
